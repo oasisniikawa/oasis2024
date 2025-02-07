3d3298a9e1f9581463b2af5d9cf732447efdb9e4
島　浩文 <shima@strawhat.onmicrosoft.com>
2025-02-07 14:50:58 +0900

    favicon設置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app:{
+    head: {
+      title: "おあしす新川",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.ico" },
+      ]
+    },
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
